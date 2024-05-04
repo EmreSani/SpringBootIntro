11acc5d7def50e37f688e0c7325b391e6a1486ba
EmreSani <emresani1998@gmail.com>
2024-05-05 00:05:55 +0300

    day2

---
+++ src/main/java/com/tpe/service/StudentService.java
@@ -0,0 +1,59 @@
+package com.tpe.service;
+
+import com.tpe.domain.Student;
+import com.tpe.exception.ConflictException;
+import com.tpe.exception.ResourceNotFoundException;
+import com.tpe.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    // Not: getAll() ***************************************************
+    public List<Student> getAll() {
+        // !!! handle etmemiz gereken bir durum var mi ?
+        return studentRepository.findAll(); // SELECT * FROM student
+    }
+
+
+    // NOt: createStudent() ********************************************
+    public void createStudent(Student student) {
+
+        // !!! kontrol etmemiz gereken bir durum var mi ? Email unique mi kontrolu
+        if(studentRepository.existsByEmail(student.getEmail())){
+            throw new ConflictException("Email is already exist");
+        }
+        studentRepository.save(student);
+    }
+
+    // Not: getStudentById with RequestParam ****************************
+    public Student findStudent(Long id) {
+
+        return studentRepository.findById(id).orElseThrow(()->
+                new ResourceNotFoundException("Student not found with id: " + id));
+    }
+
+    // Not: Delete Student ************************************************
+    public void deleteStudent(Long id) {
+
+        Student student = findStudent(id);
+        //studentRepository.delete(student);
+        studentRepository.deleteById(id);
+    }
+
+    // Not: Update Student *******************************************************************
+    public void updateStudent(Long id, StudentDTO studentDTO) {
+
+
+
+    }
+
+
+
+}
